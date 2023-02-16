8038d7900e9fefa243c3617b6d6fe3282b98a17d
Nadene381 <Nadenesamuels98@gmail.com>
2023-02-16 13:22:46 -0800

    Hello World

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Program that uses fput function to print a string
+ * Return: 0
+ */
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (0);
+}
