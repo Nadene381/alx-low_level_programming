98febe6099eb886eeef7017fbf0ad0043ea26ccb
Nadene381 <Nadenesamuels98@gmail.com>
2023-02-16 11:30:40 -0800

    Hello World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * Program that uses puts function to print a string
+ * main - Program that uses puts function to print a string
  *
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return(0);
+return (0);
 }
