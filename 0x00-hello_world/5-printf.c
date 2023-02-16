a6aa5f8ed7dc327745d984a11b48d0510587f3e8
Nadene381 <Nadenesamuels98@gmail.com>
2023-02-16 11:37:34 -0800

    Hello World

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Program that uses printf function to print a string
+ * Return: 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
