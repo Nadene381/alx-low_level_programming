934f2f85fccee3cb921ce6103dd5356307f5f79f
Nadene381 <Nadenesamuels98@gmail.com>
2023-02-16 11:05:44 -0800

    Hello World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-	String s = "\"Programming is like building a multilingual puzzle,/n ";
+	char s[] = "\"Programming is like building a multilingual puzzle,\n ";
 	puts(s);
 	return(0);
 }
