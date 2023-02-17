30cba20f45389a7803cfda144970d23b344981f2
Nadene381 <Nadenesamuels98@gmail.com>
2023-02-17 14:18:00 -0800

    C Variables

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - A program that prints numbers
+ * Return: 0
+ */
+int main(void)
+{
+int n;
+for (n = 0; n < 10; n++)
+{
+putchar (n + '0');
+if (n < 9)
+{
+putchar(",");
+putchar(' ');
+}
+putchar("\n");
+return (0);
+}
+}
