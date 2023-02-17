71d71ab451ba16bc5adf547bfe2ed5a82fe4cb76
Nadene381 <Nadenesamuels98@gmail.com>
2023-02-17 10:50:33 -0800

    C Variables

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - A program that prints alphabet in lower and uppercase
+ * Return: 0
+ */
+int main(void)
+{
+char c;
+char d;
+for (c = 'a'; c <= 'z'; ++c)
+{
+putchar(c);
+}
+for (d = 'A'; d <= 'Z'; ++d)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
