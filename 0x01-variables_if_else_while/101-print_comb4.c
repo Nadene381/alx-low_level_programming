5aa974f14bdb460698c9d00ebdbec510a9cb7526
Nadene381 <Nadenesamuels98@gmail.com>
2023-02-17 15:29:06 -0800

    C Variables

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+/**
+ * main - Program that prints numbers
+ * Return: 0
+ */
+int main(void)
+{
+int n;
+int m ;
+int o = 0;
+
+while (o < 10)
+{
+m = 0;
+while (m < 10)
+{ 
+n = 0;
+while (n < 10)
+{
+if(n != m && m != o && o < m && m < n)
+{
+putchar('0' + o);
+putchar('0' + m);
+putchar('0' + n);
+
+if (n + m + o != 9 +8 +7)
+{
+putchar(',') ;
+putchar(' ');
+}
+}
+n++;
+}
+m++;
+}
+o++;
+}
+putchar('\n');
+return (0);
+}
