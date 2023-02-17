40848c282af3d1e600ea02dcc605e26619e60a6f
Nadene381 <Nadenesamuels98@gmail.com>
2023-02-17 11:46:58 -0800

    C Variables

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Program that prints numbers 
+ * Return: 0
+ */
+int main(void) 
+{
+int c;
+for (c = 0; c < 10; c++)
+{
+printf("%d",c);
+}
+putchar('\n');
+return 0;
+}
+
