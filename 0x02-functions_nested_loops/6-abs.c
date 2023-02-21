1ceef22acb6716155323133df5edb4e8ca088808
Nadene381 <Nadenesamuels98@gmail.com>
2023-02-21 15:30:21 -0800

    C Functions

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * int _abs - Check main code
+ * Description: Function that computes absolute value of an integer
+ * Return: 1
+ * @n: integer passed in function
+ */
+int _abs(int n)
+{
+if (n >= 0)
+{
+return (r);
+}
+else
+{
+return (r * -1);
+}
+}
