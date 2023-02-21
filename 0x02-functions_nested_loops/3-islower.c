8643a217c3fc0a2f63f88f5ea0d25b1e28879258
Nadene381 <Nadenesamuels98@gmail.com>
2023-02-21 14:44:45 -0800

    C Functions

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,7 @@
  * _islower - Check main code
  * Description: Function that uses if statement
  * Return: return 1 if lowercase
- * c: variable passed in fuction
+ * @c: variable passed in fuction
  */
 int _islower(int c)
 {
@@ -12,7 +12,7 @@ int returnValue = 0;
 for (n = 'a'; n <= 'z'; n++)
 {
 if (n == c)
-returnValue = (1);
+returnValue = 1;
 }
 return (returnValue);
 }
