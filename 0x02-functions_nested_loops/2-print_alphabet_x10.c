6daf56e8006f0f9eb3896cc0fbed8ede4c25733d
Nadene381 <Nadenesamuels98@gmail.com>
2023-02-21 13:30:05 -0800

    C Functions

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
- * main - Program that prints the alphabeth in lowercase
+ * main - Function that prints the alphabeth in lowercase
  * Return: 0
  */
 void print_alphabet_x10(void)
 {
 char n;
 int m;
-for(m = 0; m < 10; m++)
+for (m = 0; m < 10; m++)
 {
 _putchar('\n');
-for(n = 'a'; n <= 'z'; n++)
+for (n = 'a'; n <= 'z'; n++)
 _putchar(n);
 }
 }
