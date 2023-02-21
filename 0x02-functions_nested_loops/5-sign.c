0f29829ca606bf36ccbe9b8e1a964cc8ab3b5456
Nadene381 <Nadenesamuels98@gmail.com>
2023-02-21 15:01:51 -0800

    C Functions

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * int print_sign - Check main code
+ * Description: Function that uses if statement
+ * Return: return 1 /-1 or 0 depending on the value of n
+ * @c: variable passed in fuction
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar(43);
+return (1);
+}
+else if (n < 0)
+{
+_putchar(45);
+return (-1);
+}
+else
+{
+_putchar(48);
+return (0);
+}
+}
