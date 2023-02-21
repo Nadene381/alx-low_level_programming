c0548fb786b5e39a78686a3d76936601d4bc4d8c
Nadene381 <Nadenesamuels98@gmail.com>
2023-02-21 12:55:48 -0800

    C Functions

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,12 +5,16 @@
  */
 int main(void)
 {
-return 0;
 }
 void print_alphabet_x10(void)
 {
 char n;
+int m = 0;
+for(m=0; m<=10; m++)
+{
+putchar('\n');
 for(n = 'a'; n<= 'z'; n++)
-_putchar(n);
-_putchar('\n');
+putchar(n);
+}
+}
 }
