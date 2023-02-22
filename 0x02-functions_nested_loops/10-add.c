0cea9ff0d569c65d59ab546445a8f1d1e6823d02
Nadene381 <Nadenesamuels98@gmail.com>
2023-02-21 16:11:10 -0800

    C Functions

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - Check main code
+ * Description: Function that add two integers
+ * Return: two variables passed in fuction 
+ * @num1: integer passed in function
+ * @num2: integer passed in function
+ */
+int add(int num1, int num2)
+{
+return (num1 + num2);
+}
