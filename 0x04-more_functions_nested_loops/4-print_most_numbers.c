012cb8b2083535467a476f2012aae61f50ff720f
Nadene381 <Nadenesamuels98@gmail.com>
2023-02-23 12:51:50 -0800

    C Debugging

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_most_numbers - Check main code
+ * Description: Function that print numbers 0 - 9 (dont print 2 & 4)
+ * Return: 0
+ */
+void print_most_numbers(void)
+{
+int n;
+for (n = 48; n <= 57; n++)
+{
+if (n != 2 && n != 4)
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
