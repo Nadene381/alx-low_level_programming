87e893c12db35676206a4ae68606ffb31caa1f12
Nadene381 <Nadenesamuels98@gmail.com>
2023-02-23 12:22:12 -0800

    C Functions & loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - Check main code
+ * Description: Function that multiply 2 integers
+ * Return: return 0
+ * @a: variable passed in fuction
+ * @b: variable passed in fuction
+ */
+int mul(int a, int b)
+{
+ return(a*b);
+}
