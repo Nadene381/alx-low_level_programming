64b1effef1aaa1d3f73f97505ff9d5f14dc7c06c
Nadene381 <Nadenesamuels98@gmail.com>
2023-02-23 14:41:19 -0800

    C Functions & loops

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Check code
+ * Description: Function that prints numbers from 1 -100
+ * Return: Fizz, Buzz or fizzBuzz
+ */
+int main (void)
+{
+int n;
+for (n = 1; n <= 100; n++)
+{
+if (((n % 3) != 0)  && ((n % 5) != 0))
+printf("%d ",n);
+if (((n % 3) == 0)  && ((n % 5) == 0))
+printf("Fizzbuzz ");
+else if ((n % 3) == 0)
+printf("Fizz ");
+else if ((n % 5) == 0)
+printf("Buzz ");
+}
+return (0);
+putchar('\n');
+}
