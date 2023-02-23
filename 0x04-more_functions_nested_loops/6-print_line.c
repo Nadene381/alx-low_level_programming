34283c4bf7851681c89d9c9454c92582eae66ecd
Nadene381 <Nadenesamuels98@gmail.com>
2023-02-23 14:08:29 -0800

    C Functions & loops

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_line - Check main code
+ * Description: Function that prints straight line (_)
+ *@n: variable passed in fuction
+ * Return: 0
+ */
+void print_line(int n)
+{
+int m;
+for (m = 0; m < n; m++)
+{
+_putchar(95);
+}
+_putchar('\n');
+}
