11c3df3a7b2b0a5da790df4d9fb3a833d6cedfe6
Nadene381 <Nadenesamuels98@gmail.com>
2023-02-23 15:03:51 -0800

    C Functions & loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,13 +10,21 @@ int n;
 for (n = 1; n <= 100; n++)
 {
 if (((n % 3) != 0)  && ((n % 5) != 0))
+{
 printf("%d ", n);
+}
 else if (((n % 3) == 0)  && ((n % 5) == 0))
+{
 printf("Fizzbuzz ");
+}
 else if ((n % 3) == 0)
+{
 printf("Fizz ");
+}
 else if ((n % 5) == 0)
+{
 printf("Buzz ");
+}
 }
 printf("\n");
 return (0);
