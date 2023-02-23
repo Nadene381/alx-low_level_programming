c0f0d8d0a8d2cdfa62c7c11d49987573e0326bec
Nadene381 <Nadenesamuels98@gmail.com>
2023-02-23 06:03:46 -0800

    C Functions & loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isupper - Check main code
+ * Description: Function that uses if statement
+ * Return: return 1 if uppercase and 0 if lowercase
+ * @c: variable passed in fuction
+ */
+int _isupper(int c)
+{
+char i;
+for (i = 'A'; i <= 'Z'; i++)
+{
+if (c == i)
+return (1);
+else if (c != i)
+return (0);
+}
+return (i);
+}
