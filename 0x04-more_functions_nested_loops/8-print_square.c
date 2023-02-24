9907eaa6149d2b474902eec6049120500a848116
Nadene381 <Nadenesamuels98@gmail.com>
2023-02-23 16:01:54 -0800

    C Functions & loops

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_square- Check main code
+ * Description: Function that prints #
+ * @size: integer passed in function
+ * Return: 0
+ */
+void print_square(int size)
+{
+int n;
+int m;
+for (n = 0; n < size; n++)
+{
+for (m = 0; m < size; m++)
+{
+_putchar(35);
+}
+if (i != size - 1);
+_putchar('\n');
+}
+_putchar('\n');
+}
