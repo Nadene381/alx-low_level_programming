7409c0a99fe80bc2dc5bfa600dcf5737fbcdbf48
Nadene381 <Nadenesamuels98@gmail.com>
2023-02-23 16:54:06 -0800

    C Functions & loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,11 +13,11 @@ if ((n % 3 == 0) && (n % 5 == 0))
 {
 printf("Fizzbuzz ");
 }
-else if ((n % 3) == 0)
+else if (n % 3 == 0)
 {
 printf("Fizz ");
 }
-else if ((n % 5) == 0)
+else if (n % 5 == 0)
 {
 printf("Buzz ");
 }
