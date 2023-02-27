5a1da80ef6d535372c9b7822db81e6b42b80878c
Nadene381 <Nadenesamuels98@gmail.com>
2023-02-27 03:35:03 -0800

    C pointers & arrays

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * reset_to_98 - Check main code
+ * Description: Function that takes a pointer to an int
+ * Return: 1
+ * @n: variable passed in fuction
+ */
+void reset_to_98(int *n)
+{
+int *p = *(&n);
+*p = 98;
+return (1);
+}
