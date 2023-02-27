d9fb27c29612382cd1d098480d61775422d6e8f6
Nadene381 <Nadenesamuels98@gmail.com>
2023-02-27 14:55:50 -0800

    C pointers & arrays

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * _atoiCheck main code
+ * Description: Function that convert int to string
+ * Return: 1 but if no numbers in string return 0
+ * @s: variable passed in fuction
+ */
+int _atoi(char *s)
+{
+int i = 0;
+int n = 0;
+int m = 1;
+while ((s[i] < '0' || s[i] > '9') && s[i] != 0)
+{
+if (s[i] == '-')
+m *= -1;i++;
+}
+while ((s[i] >= '0' && s[i] <= '9') && s[i] != 0)
+{
+if (n >= 0)
+{
+n = n * 10 - (s[i] - '0');
+i++;
+}
+else
+{
+n = n * 10 - (s[i] - '0');
+i++;
+}
+}
+m *= -1;
+return (n * m);
+}
