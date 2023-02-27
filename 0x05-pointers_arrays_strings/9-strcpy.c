c8752c33cf68c0df335722e90f191fed93ac2fcb
Nadene381 <Nadenesamuels98@gmail.com>
2023-02-27 11:54:11 -0800

    C pointers & arrays

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
  * print_array- Check main code
- * Description: Function that copies string pointed by src to bufffer pointed by dest
+ * Description: Function that copies string to bufffer 
  * Return: pointer dest
  * @dest: variable passed in fuction
  * @src: variable passed in fuction
  */
-char *_strcpy(char *dest, char *src);
+char *_strcpy(char *dest, char *src)
 {
 int n = 0;
 while (n >= 0)
