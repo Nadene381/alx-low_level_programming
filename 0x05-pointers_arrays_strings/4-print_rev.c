5e7c7bde150c32c479287486cd995fa3f3f740b4
Nadene381 <Nadenesamuels98@gmail.com>
2023-02-27 11:01:18 -0800

    C pointers & arrays

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -13,14 +13,14 @@ char *n, *m, temp;
 lenght = strlen(s);
 n = s;
 m = s;
-for (i = 0; i < lenght - 1; i++) 
+for (i = 0; i < lenght - 1; i++)
 m++;
-for (i = 0; i < lenght/2; i++)
+for (i = 0; i < lenght / 2; i++)
 {
 temp = *m;
 *m = *n;
 *n = temp;
 n++;
-m--; 
+m--;
 }
 }
