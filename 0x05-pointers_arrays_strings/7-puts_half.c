6870afd0d60ec314d75b7a692d51bf9b4ca46f60
Nadene381 <Nadenesamuels98@gmail.com>
2023-02-27 12:31:53 -0800

    C pointers & arrays

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * puts_half - Check main code
+ * Description: Function that prints half of a string
+ * Return: 1
+ * @str: variable passed in fuction
+ */
+void puts_half(char *str)
+{
+int m, i;
+char half_of_string[10];
+int length_of_the_string = strlen(str);
+int n = (length_of_the_string -1) / 2;
+if (str[i] != '\0')
+{
+for(i = n+1, m = 0; i <= length_of_the_string; i++, m++) 
+{
+half_of_string[m] = str[i];
+}
+printf("%s", half_of_string);
+}
+}
