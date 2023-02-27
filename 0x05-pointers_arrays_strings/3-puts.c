0251faa65483f5e858400b1468bfe60df1aed282
Nadene381 <Nadenesamuels98@gmail.com>
2023-02-27 06:59:51 -0800

    C pointers & arrays

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts - Check main code
+ * Description: Function that prints a string
+ * Return: 1
+ * @str: variable passed in fuction
+ */
+void _puts(char *str);
+{
+while (*str != '\0') 
+{
+putchar(*str);
+str++;
+}
+}
