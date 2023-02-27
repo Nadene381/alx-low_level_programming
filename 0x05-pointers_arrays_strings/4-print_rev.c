c45259b5cd552cf17724428c16fe64aa5f761753
Nadene381 <Nadenesamuels98@gmail.com>
2023-02-27 10:55:06 -0800

    C pointers & arrays

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_rev - Check main code
+ * Description: Function that prints a string in reverse
+ * Return: 1
+ * @s: variable passed in fuction
+ */
+void print_rev(char *s)
+{
+int lenght;
+int i;  
+char *n, *m, temp;  
+lenght = strlen (s);  
+n = s;  
+m = s;  
+for (i = 0; i < lenght - 1; i++)  
+m++;  
+for (i = 0; i < lenght/2; i++)  
+{  
+temp = *m;  
+*m = *n;  
+*n = temp;  
+n++;  
+m--;  
+}
+}
