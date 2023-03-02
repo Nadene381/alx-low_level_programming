ae04d85040fdd8ed0921b60ceb8f857996ae152c
Nadene381 <Nadenesamuels98@gmail.com>
2023-03-02 08:31:03 -0800

    C pointers, arrays & strings

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * reverse_array- Check main code
+ * Description: Function that reverses the content of an array of integers.
+ * Return: 0
+ * @a: variable passed in fuction
+ * @n: variable passed in fuction
+ */
+void reverse_array(int *a, int n)
+{
+for (int m = 0; m < n/2; m++)
+{
+int temp = a[m];
+a[m] = a[n - 1 - m];
+a[n - 1 - m] = temp;
+}
+}
