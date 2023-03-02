d067e9a837d7d3ebe7b0de28cf7734fc922287f6
Nadene381 <Nadenesamuels98@gmail.com>
2023-03-02 07:15:06 -0800

    C pointers, arrays & strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * *_strcat- Check main code
+ * Description: Function that concatenates two string
+ * Return: 1
+ * @dest: variable passed in fuction
+ * @src: variable passed in fuction
+ */
+char *_strcat(char *dest, char *src)
+{
+int lenght = strlen(dest);
+int i;
+for (i = 0 ; i < src[i] != '\0' ; i++)
+dest[lenght + i] = src[i];
+dest[lenght + i] = '\0';
+return dest;
+}
