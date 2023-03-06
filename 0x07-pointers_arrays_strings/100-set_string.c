14584ea4919251c5c7900ba61ba3401edfa62113
Nadene381 <Nadenesamuels98@gmail.com>
2023-03-06 12:37:09 -0800

    C pointers, arrays & strings

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * set_string - Check main code
+ * Description: A function that sets the value of a pointer to a char.
+ * Return: 1
+ * @s: variable passed in fuction
+ * @to: variable passed in fuction
+ */
+void set_string(char **s, char *to)
+{
+*s = to;
+}
