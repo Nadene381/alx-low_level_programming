4122f0c986f361651e99583096643db81cc5d1c7
Nadene381 <Nadenesamuels98@gmail.com>
2023-03-06 11:43:14 -0800

    C pointers, arrays & strings

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_chessboardCheck main code
+ * print_chessboard - Check main code
  * Description: A function that prints the chessboard..
  * Return: 0
  * @a: variable passed in fuction
@@ -9,7 +9,7 @@ void print_chessboard(char (*a)[8])
 {
 int i = 0;
 int lenght = 0;
-for(i = 0; i < 64; i++)
+for (i = 0; i < 64; i++)
 {
 if (i % 8 == 0 && i != 0)
 {
