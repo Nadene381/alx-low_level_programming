913bedacdaedf8a1ad3ee57bbc9b4d8873022c5b
Nadene381 <Nadenesamuels98@gmail.com>
2023-03-08 11:19:15 -0800

    C Recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -5,14 +5,6 @@
  * Return: 1
  * @n: variable passed in fuction
  */
-
-/**
- * is_prime_number - Check main code
- * Description: A function that checks if number is 0 or 1
- * Return: 1
- * @m: variable passed in fuction
- * @i: variable passed in fuction
- */
 int getPrimNum(int m, int i)
 {
 if (m == i)
@@ -21,7 +13,13 @@ if (m % i == 0)
 return (0);
 return (getPrimNum(m, i + 1));
 }
-
+/**
+ * is_prime_number - Check main code
+ * Description: A function that checks if number is 0 or 1
+ * Return: 1
+ * @m: variable passed in fuction
+ * @i: variable passed in fuction
+ */
 int is_prime_number(int n)
 {
 if (n <= 1)
