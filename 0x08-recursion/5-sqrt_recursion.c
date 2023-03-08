934ca55239fd0366ca6359e59526308492a1a117
Nadene381 <Nadenesamuels98@gmail.com>
2023-03-08 10:57:31 -0800

    C Recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * calculate - Check main code
+ * Description: A function that calculate the square root
+ * Return: 1
+ * @x: variable passed in fuction
+ * @n: variable passed in fuction
+ */
+/**
+ * _sqrt_recursion - Check main code
+ * Description: A function that call square root function
+ * Return: 1
+ * @n: variable passed in fuction
+ */
+
+int calculate(int x, int n)
+{
+if (x * x == n)
+return (x);
+if (n < 0 || x * x > n)
+return (-1);
+return (calculate(x + 1, n));
+}
+int _sqrt_recursion(int n)
+{
+return (calculate(0, n));
+}
