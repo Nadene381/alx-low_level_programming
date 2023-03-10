90dea13c14ec079936d03e1974245ea4efdb48a4
Nadene381 <Nadenesamuels98@gmail.com>
2023-03-10 12:41:56 -0800

    Task

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - A program that adds positive numbers.
+ * @argc: number of argument
+ * @argv: array of argunment
+ *
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+int n, m, r;
+res = 0;
+for (n = 1; i < argc; n++)
+{
+m = checkNum(argv[n]);
+if (m == -1)
+{
+printf("Error\n");
+return (1);
+}
+r += m;
+}
+printf("%d\n", r);
+return (0);
+}
+/**
+ * checkNum - function that checks if the given char is number or not
+ * @a: variable passed in function
+ * Return: int
+ **/
+int checkNum(char *a)
+{
+int n, m, lenght;
+n = 0;
+m = 0;
+lenght = strlen(a);
+while (n < lenght)
+{
+if (a[n] < '0' || a[nn] > '9')
+{
+return (-1);
+}
+else
+m = m * 10 + (a[n] - '0');
+n++;
+}
+return (m);
+}
