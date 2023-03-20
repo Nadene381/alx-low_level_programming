959e48235775064770280f7581f27e111c8d2e32
Nadene381 <Nadenesamuels98@gmail.com>
2023-03-20 08:25:45 -0700

    Structures & typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ *struct dog - Short description
+ *@name: First member
+ *@age: Second member
+ *@owner: Third member
+ *Description: Larger description
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
