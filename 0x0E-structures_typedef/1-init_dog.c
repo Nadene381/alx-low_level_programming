586e6f868a68079320cafe32dbf39a38e9be4289
Nadene381 <Nadenesamuels98@gmail.com>
2023-03-20 09:11:24 -0700

    Structures & typedef

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * init_dog - A function that initialize a variable of type struct dog
+ * @d: variable passed in function
+ * @name: variable passed in function
+ * @age: variable passed in function
+ * @owner: variable passed in function
+ * Return: 0
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
