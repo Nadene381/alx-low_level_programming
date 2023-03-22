a69ce405f9403550d22c9282cf9eed3cfba545c1
Nadene381 <Nadenesamuels98@gmail.com>
2023-03-22 09:27:18 -0700

    Function pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,8 +10,8 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-unsigned int i;
-if (array == 0 || action == 0 || size >= 1)
+size_t i;
+if (array == NULL || action == NULL || size > 1)
 {
 for (i = 0; i < size; i++)
 {
