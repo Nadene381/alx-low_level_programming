a1eaa3f56af6082d1d04e8b41ee6c8f93b1a6321
Nadene381 <Nadenesamuels98@gmail.com>
2023-04-04 16:17:12 -0700

    more linked list

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -3,22 +3,22 @@
 #include <stdlib.h>
 #include <stddef.h>
 /**
-* sum_listint - A function that returns the sum of all the data 
-* @head: pointer passed in function 
+* sum_listint - A function that returns the sum of all the data
+* @head: pointer passed in function
 * Return: nothing if linked list is empty
-* 
+*
 */
 int sum_listint(listint_t *head)
 {
 listint_t *new_Node = head;
-int sumOfData = 0; 
-if (head == NULL) 
-{ 
+int sumOfData = 0;
+if (head == NULL)
+{
 }
 for (; new_Node != NULL; )
 {
 sumOfData += new_Node->n;
 new_Node = new_Node->next;
-} 
+}
 return (sumOfData);
 }
