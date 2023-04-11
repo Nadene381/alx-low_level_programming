2032fd5758a3610c3419c390ce7035dc3ab9e610
Nadene381 <Nadenesamuels98@gmail.com>
2023-04-11 15:23:39 -0700

    Bit manipulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -13,6 +13,10 @@ numberOfBits =  (sizeof(unsigned int) * 8 -1);
 do {
 while(numberOfBits >= 0 && !(n &(1u <<numberOfBits )) )
 numberOfBits--;
+if (numberOfBits == 0)
+{
+printf("0\n");
+}
 for (; numberOfBits >= 0; numberOfBits--)
 {
 printf("%ld", (n >> numberOfBits) & 1);
