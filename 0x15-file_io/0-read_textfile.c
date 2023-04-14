0a55424c668efaac574d9422a4c63b222bb21f0d
Nadene381 <Nadenesamuels98@gmail.com>
2023-04-14 12:09:09 -0700

    File I/O

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+* read_textfile - function that reads a text file and prints it
+* @filename: variable passed in function
+* @letters: variable passed in function
+* Return: actual number of letters it could read and print or 0 when error
+*
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+int myFile;
+char *buffer;
+ssize_t characterRead = 0;
+ssize_t readOutput = 0;
+ssize_t characterWrite;
+while (filename == NULL)
+{
+return (0);
+}
+myFile = open(filename, O_RDONLY);
+while (myFile == -1)
+{
+return (0);
+}
+buffer = (char *) malloc(letters * sizeof(char));
+if (buffer == NULL)
+{
+close(myFile);
+return (0);
+}
+while ((size_t)characterRead < letters && (readOutput = read(myFile, buffer + characterRead, letters - characterRead)) != 0)
+{
+characterRead += readOutput;
+}
+characterWrite  = write(STDOUT_FILENO, buffer, characterRead);
+close(myFile);
+free(buffer);
+if (characterWrite != characterRead)
+{
+return (0);
+}
+return (characterRead);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#define MAX_BUFFER_SIZE 1024
+
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
