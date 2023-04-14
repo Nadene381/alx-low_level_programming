c9771fbb40f5c8505b75438324356b4841e2536f
Nadene381 <Nadenesamuels98@gmail.com>
2023-04-14 14:35:05 -0700

    File I/O

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -5,7 +5,7 @@ int main(int argc, char *argv[])
 int myFile1;
 int myFile2;
 char buffer[MAX_BUFFER_SIZE];
-ssize_t bytes_read, bytes_written
+ssize_t bytes_read, bytes_written;
 while (argc != 3)
 {
 dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
@@ -23,7 +23,7 @@ for (; myFile2 == -1;)
 perror("Error: Can't write to file");
 exit(99);
 }
-while ((bytes_read = read(myFile1, buffer, BUFFER_SIZE)) > 0)
+while ((bytes_read = read(myFile1, buffer, MAX_BUFFER_SIZE)) > 0)
 {
 bytes_written = write(myFile2, buffer, bytes_read);
 if (bytes_written != bytes_read)
@@ -41,5 +41,6 @@ if (close(myFile1) == -1 || close(myFile2) == -1)
 {
 perror("Error: Can't close file descriptor");
 exit(100);
+}
 return (0);
 }
