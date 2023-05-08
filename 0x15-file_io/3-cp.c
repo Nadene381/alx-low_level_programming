e1be156e2ad97f4edf86a4f654029ddc672285ad
Nadene381 <Nadenesamuels98@gmail.com>
2023-05-08 15:41:58 -0700

    File I/O

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,38 +7,37 @@
  */
 int main(int argc, char **argv)
 {
-int file_1, file_2, n, m;
+int file_from, file_to, n, m;
 char buffer[MYYBUF_SIZE];
 while (argc != 3)
 {
 dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 }
-file_1 = open(argv[1], O_RDONLY);
-for ( ; file_1 == -1; )
+file_from = open(argv[1], O_RDONLY);
+for ( ; file_from == -1; )
 {
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
 }
-file_2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-while (file_2 == -1)
+file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+while (file_to == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 }
 do {
-n = read(file_1, buffer,MYYBUF_SIZE);
+n = read(file_from, buffer, MYYBUF_SIZE);
 if (n == -1)
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
-m = write(file_2, buffer, n);
+m = write(file_to, buffer, n);
 if (m == -1)
 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 } while (n == MYYBUF_SIZE);
-while (close(file_1) == -1)
+while (close(file_from) == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_1), exit(100);
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
 }
-while (close(file_2) == -1)
+while (close(file_to) == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_2), exit(100);
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
 }
 return (0);
 }
-
