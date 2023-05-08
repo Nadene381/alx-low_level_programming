757f4b7fee172165b838804bc07e2f6b6c816dab
Nadene381 <Nadenesamuels98@gmail.com>
2023-05-08 14:03:46 -0700

    File I/O

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * read_textfile - function that that reads a text file and prints it
+ *@letters: variable passed in function
+ *@filename: pointer passed in function
+ *Return: number of letters it could read and print
+ *
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+ssize_t n, m;
+char myBuff[letters];
+int getmyFile;
+if (filename == NULL)
+{
+return (0);
+}
+getmyFile = open(filename, O_RDONLY);
+if (getmyFile == -1)
+{
+return (0);
+}
+n = read(getmyFile, myBuff, letters);
+if (n == -1)
+{
+close(getmyFile);
+return (0);
+}
+m = write(STDOUT_FILENO, myBuff, n);
+close(getmyFile);
+return ((m == -1 || m != n) ? 0 : n);
+}
