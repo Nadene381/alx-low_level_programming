21bbd4ae38a2a235d1a2542ebcb0e66a26bd17b8
Nadee381 <Nadenesamuels98@gmail.com>
2023-06-15 09:33:05 -0700

    Doubly linked lists

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,11 +1,10 @@
 #include "lists.h"
 /**
-* add_dnodeint_end - Returns the sum of all the data of a linked list.
-* @head: pointer passed in function
-* @n: variable passed in function
-* Return: 0 if the list is empty
-*/
-dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+ * sum_dlistint - Returns the sum of all the data of a linked list.
+ * @head: pointer passed in function
+ * Return: 0 if the list is empty
+ */
+int sum_dlistint(dlistint_t *head)
 {
 dlistint_t *newNode = malloc(sizeof(dlistint_t));
 dlistint_t *c = *head;
