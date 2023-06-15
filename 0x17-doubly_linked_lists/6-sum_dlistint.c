816a5d4587193084b84dd274fb0dd2598c21dd30
Nadee381 <Nadenesamuels98@gmail.com>
2023-06-15 09:36:51 -0700

    Doubly linked lists

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,26 +1,17 @@
 #include "lists.h"
 /**
- * sum_dlistint - Returns the sum of all the data of a linked list.
- * @head: pointer passed in function
- * Return: 0 if the list is empty
- */
+* sum_dlistint - Returns the sum of all the data of a linked list.
+* @head: pointer passed in function
+* Return: 0 if the list is empty
+*/
 int sum_dlistint(dlistint_t *head)
 {
-dlistint_t *newNode = malloc(sizeof(dlistint_t));
-dlistint_t *c = *head;
-while (newNode == NULL)
-return (NULL);
-newNode->n = n;
-newNode->next = NULL;
-if (*head == NULL)
+int sum = 0;
+dlistint_t *c = head;
+while (c != NULL)
 {
-newNode->prev = NULL;
-*head = newNode;
-return (newNode);
-}
-while (c->next != NULL)
+sum += c->n;
 c = c->next;
-c->next = newNode;
-newNode->prev = c;
-return (newNode);
+}
+return (sum);
 }
