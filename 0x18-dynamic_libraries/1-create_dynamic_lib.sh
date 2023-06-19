4309396c4f7e285121c46b3a73b4759128657487
Nadee381 <Nadenesamuels98@gmail.com>
2023-06-19 10:29:40 -0700

    Dynamic libraries

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-all_my_c_files=$(find . -maxdepth 1 -type f -name "*.c" | sort)
-o_file=""
-for myFile in $all_my_c_files; do
-    object_file=$(basename "$myFile" ".c").o
-    gcc -c -fPIC "$myFile" -o "o_file"
-    o_file+=" $o_file"
-done
-gcc -shared -o liball.so o_file
-rm -f o_file
+gcc -g -fPIC -Wall -Werror -Wextra -pedantic *.c -shared -o liball.so
